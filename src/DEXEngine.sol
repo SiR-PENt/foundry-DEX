097a095bd2f8e7753a31ab74d46c62ca3eb3f609
Olasunkanmi <olasunkanmiibalogun@gmail.com>
2024-09-30 23:42:09 +0100

    revamp project structure

--- src/DEXEngine.sol
+++ src/DEXEngine.sol
@@ -39,137 +39,164 @@ contract DEXEngine is ReentrancyGuard {
     using SafeTransferLib for ERC20;
 
     error DEXEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+    error DEXEngine__NeedsMoreThanZero();
     error DEXEngine__TransferFailed();
     error DEXEngine__ToleranceLevelBreached(); // this means the values are apart by a wide range
     error DEXEngine__MintFailed();
+    error DEXEngine__PoolDoesNotExists();
 
     struct Pool {
-        uint256 reserveTokenA; // how much is in the reserve of the first token
-        uint256 reserveTokenB; // reserve for the second token
-        uint256 totalSupplyOfLpTokens; // total supply of the lpTokens based on the reserve
+        uint256 baseTokenReserve; // how much is in the reserve of the first token
+        uint256 quoteTokenReserve; // reserve for the second token
+        uint256 totalSupplyOfLpTokens; // total supply of the lpTokens based on the reserve (thia would be in usd value?)
         mapping(address lp => uint256 amount) tokenBalanceOfLp; // address of each liquidity provider to amount
     }
 
+    address[] s_baseTokens;
+    address s_quoteToken;
+
+    mapping(address quoteToken => address priceFeedAddress) s_quoteTokenPrice;
+    mapping(address baseToken => address priceFeedAddress) private s_baseTokenPrice;
+    mapping(address baseToken => address quoteToken) liquidityPools;
     // now we want to create a mapping of liquidity pools
-    mapping(address tokenAAddress => mapping(address tokenBAddress => Pool pool)) liquidityPools;
+    mapping(address tokenAAddress => mapping(address tokenBAddress => Pool pool)) liquidityPoolInfo;
 
     uint256 constant ADDITIONAL_FEED_PRECISION = 1e10;
     uint256 constant PRECISION = 1e18;
 
     DEXToken private immutable i_dexToken;
-    mapping(address tokenAddress => address priceFeedAdress) private s_priceFeeds;
+    address[] s_tokenAddresses;
 
     event LiquidityAdded(address indexed tokenA, address indexed tokenB, uint256 amountA, uint256 amountB);
 
-    modifier tokensDifferenceChecker(address tokenA, address tokenB) {
-        require(tokenA != tokenB, "Tokens must be different");
+    modifier moreThanZero(uint256 amountA, uint256 amountB) {
+        if (amountA == 0 || amountB == 0) {
+            revert DEXEngine__NeedsMoreThanZero();
+        }
         _;
     }
 
-    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dexTokenAddress) {
-        if (tokenAddresses.length != priceFeedAddresses.length) {
+    modifier poolExists(address baseToken) {
+        bool exists = false;
+        for (uint256 i = 0; i < s_baseTokens.length; i++) {
+            if (s_baseTokens[i] == baseToken) {
+                exists = true;
+                break;
+            }
+        }
+        if (!exists) {
+            revert DEXEngine__PoolDoesNotExists(); // Revert if the pool doesn't exist
+        }
+        _;
+    }
+
+    constructor(
+        address[] memory baseTokens,
+        address[] memory baseTokenPriceFeedAddresses,
+        address quoteToken,
+        address quoteTokenPriceFeedAddress,
+        address dexTokenAddress
+    ) {
+        if (baseTokens.length != baseTokenPriceFeedAddresses.length) {
             revert DEXEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
         }
 
-        for (uint256 i = 0; i < tokenAddresses.length; i++) {
-            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        s_quoteToken = quoteToken;
+        s_quoteTokenPrice[s_quoteToken] = quoteTokenPriceFeedAddress; // map the quoteToken to its price feed
+
+        for (uint256 i = 0; i < baseTokens.length; i++) {
+            s_baseTokens.push(baseTokens[i]); //list of baseTokens addresses
+            liquidityPools[baseTokens[i]] = s_quoteToken; //create a pool by mapping them to the quoteToken
+            s_baseTokenPrice[baseTokens[i]] = baseTokenPriceFeedAddresses[i]; //
         }
 
         i_dexToken = DEXToken(dexTokenAddress);
     }
-    
+
     /**
      *
-     * @param tokenAAddress first token in the liquidity pool pair
-     * @param tokenBAddress second token in the liquidity pool pair
-     * @param amountA amount to deposit in the first token
-     * @param amountB amount to deposit to the second token
+     * @param baseToken address of the base token
+     * @param baseTokenAmount amount to deposit in the first token
+     * @param quoteTokenAmount amount to deposit to the second token
      * @notice this function will deposit users collateral and mint LP tokens for the user
      */
-
-
-    function depositLiquidity(address tokenAAddress, address tokenBAddress, uint256 amountA, uint256 amountB)
+    function depositLiquidity(address baseToken, uint256 baseTokenAmount, uint256 quoteTokenAmount)
         public
-        tokensDifferenceChecker(tokenAAddress, tokenBAddress)
+        moreThanZero(baseTokenAmount, quoteTokenAmount)
+        poolExists(baseToken)
         nonReentrant
     {
-
-        (tokenAAddress, tokenBAddress, amountA, amountB) = _orderTokensLexicographically(tokenAAddress, tokenBAddress, amountA, amountB);
-
-        uint256 valueA = getUsdValueOfToken(tokenAAddress, amountA);
-        uint256 valueB = getUsdValueOfToken(tokenBAddress, amountB);
+        uint256 valueA = getUsdValueOfBaseTokenDonated(baseToken, baseTokenAmount);
+        uint256 valueB = getUsdValueOfQuoteTokenDonated(quoteTokenAmount);
 
         if (valueA <= valueB * 99 / 100 || valueA >= valueB * 101 / 100) revert DEXEngine__ToleranceLevelBreached();
 
-        Pool storage pool = liquidityPools[tokenAAddress][tokenBAddress];
-        pool.reserveTokenA += amountA;
-        pool.reserveTokenB += amountB;
-        uint256 poolBalance = getPoolValueInUsd(tokenAAddress, tokenBAddress);
+        Pool storage pool = liquidityPoolInfo[baseToken][s_quoteToken];
+        pool.baseTokenReserve += baseTokenAmount;
+        pool.quoteTokenReserve += quoteTokenAmount;
+        uint256 poolBalance = getPoolValueInUsd(baseToken, s_quoteToken);
         uint256 amountOfTokensDepositedByLpInUsd = valueA + valueB;
         pool.totalSupplyOfLpTokens = i_dexToken.totalSupply();
 
         if (pool.totalSupplyOfLpTokens == 0) {
             // mint dextoken for user and update the state
-            mintDEXToken(amountOfTokensDepositedByLpInUsd);
+            _mintDEXToken(amountOfTokensDepositedByLpInUsd);
             pool.tokenBalanceOfLp[msg.sender] += amountOfTokensDepositedByLpInUsd;
         } else {
             uint256 lpToken = (amountOfTokensDepositedByLpInUsd * pool.totalSupplyOfLpTokens) / poolBalance;
-            mintDEXToken(lpToken);
+            _mintDEXToken(lpToken);
             pool.tokenBalanceOfLp[msg.sender] += lpToken;
         }
 
-        ERC20 tokenA = ERC20(tokenAAddress);
-        ERC20 tokenB = ERC20(tokenAAddress);
+        ERC20 erc20baseToken = ERC20(baseToken);
+        ERC20 erc20quoteToken = ERC20(s_quoteToken);
 
-        tokenA.safeTransferFrom(msg.sender, address(this), amountA); 
-        tokenB.safeTransferFrom(msg.sender, address(this), amountB); 
+        erc20baseToken.safeTransferFrom(msg.sender, address(this), baseTokenAmount);
+        erc20quoteToken.safeTransferFrom(msg.sender, address(this), quoteTokenAmount);
 
-        emit LiquidityAdded(tokenAAddress, tokenBAddress, amountA, amountB);
+        emit LiquidityAdded(baseToken, s_quoteToken, baseTokenAmount, quoteTokenAmount);
     }
 
-    function mintDEXToken(uint256 amountDscToMint) internal nonReentrant {
+    function _mintDEXToken(uint256 amountDscToMint) private nonReentrant {
         bool minted = i_dexToken.mint(msg.sender, amountDscToMint);
         if (!minted) {
             revert DEXEngine__MintFailed();
         }
     }
     /**
-     * @notice we want a situation where we want to check if the user has lp tokens 
+     * @notice we want a situation where we want to check if the user has lp tokens
      * the user also gets an allocation proportional to his lp token
      * burns the user's lp token
-     * 
+     *
      */
 
-    function withdrawLiquidity() public nonReentrant {
+    function withdrawLiquidity() public nonReentrant {}
 
+    // this will return in e18
+    function getUsdValueOfBaseTokenDonated(address token, uint256 amount) public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_baseTokenPrice[token]); // get the priceFeed of the token via chainlink
+        (, int256 price,,,) = priceFeed.staleCheckLatestRoundData();
+        return ((uint256(price) * ADDITIONAL_FEED_PRECISION) * amount) / PRECISION;
     }
 
     // this will return in e18
-    function getUsdValueOfToken(address token, uint256 amount) public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]); // get the priceFeed of the token via chainlink
+    function getUsdValueOfQuoteTokenDonated(uint256 amount) public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_quoteTokenPrice[s_quoteToken]); // get the priceFeed of the token via chainlink
         (, int256 price,,,) = priceFeed.staleCheckLatestRoundData();
         return ((uint256(price) * ADDITIONAL_FEED_PRECISION) * amount) / PRECISION;
     }
 
     function getPoolValueInUsd(address tokenA, address tokenB) public view returns (uint256) {
-    // Convert each reserve to USD using Chainlink price feeds
-    uint256 reserveAInUsd = getUsdValueOfToken(tokenA, liquidityPools[tokenA][tokenB].reserveTokenA);
-    uint256 reserveBInUsd = getUsdValueOfToken(tokenB, liquidityPools[tokenA][tokenB].reserveTokenB);
+        // Convert each reserve to USD using Chainlink price feeds
+        uint256 reserveAInUsd =
+            getUsdValueOfBaseTokenDonated(tokenA, liquidityPoolInfo[tokenA][tokenB].baseTokenReserve);
+        uint256 reserveBInUsd =
+            getUsdValueOfBaseTokenDonated(tokenB, liquidityPoolInfo[tokenA][tokenB].quoteTokenReserve);
 
-    // Sum the USD values to get the total pool value
-    uint256 totalPoolValueInUsd = reserveAInUsd + reserveBInUsd;
-    return totalPoolValueInUsd;
+        // Sum the USD values to get the total pool value
+        uint256 totalPoolValueInUsd = reserveAInUsd + reserveBInUsd;
+        return totalPoolValueInUsd;
     }
 
-    function getTotalSupplyOfLpTokensInAPool() public view returns (uint256) {
-
-    }
-
-    function _orderTokensLexicographically(address tokenA, address tokenB, uint256 amountA, uint256 amountB) private pure returns (address tokenAAddress, address tokenBAddress, uint256 tokenAAmount, uint256 tokenBAmount) {
-        if (tokenA > tokenB) {
-          (tokenA, tokenB) = (tokenB, tokenA);  // Swap the tokens if they are out of order
-          (amountA, amountB) = (amountB, amountA);  // Swap the amounts to match the tokens
-          return (tokenA, tokenB, amountA, amountB);
-        }
-    }
+    function getTotalSupplyOfLpTokensInAPool() public view returns (uint256) {}
 }
